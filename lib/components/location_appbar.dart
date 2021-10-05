cc0c27578af131b603408fcd40d85b6562a5866f
Chee Hong <c.h.wong2606@gmail.com>
2021-10-05 20:41:13 +0800

    Location info for booking appbar
    +Location appbar uses current location of device when
    `withCurrentLocation` is set to true.

--- lib/components/location_appbar.dart
+++ lib/components/location_appbar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:charg_ev/services/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
 
 class LocationAppBar extends StatefulWidget {
   const LocationAppBar({
@@ -15,21 +17,36 @@ class LocationAppBar extends StatefulWidget {
 }
 
 class _LocationAppBarState extends State<LocationAppBar> {
+  String location = "loading";
+
+  @override
+  void initState() {
+    super.initState();
+
+    determineLastKnownPosition()
+        .then((pos) => placemarkFromCoordinates(pos.latitude, pos.longitude))
+        .then((placemark) {
+          setState(() {
+            location = placemark[0].street;
+          });
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-
     // fetch dimensions of phone
     Size size = MediaQuery.of(context).size;
 
     return Container(
       width: size.width,
-      padding: EdgeInsets.fromLTRB(size.width * 0.05, size.height * 0.03, size.width * 0.05, 0),
+      padding: EdgeInsets.fromLTRB(
+          size.width * 0.05, size.height * 0.03, size.width * 0.05, 0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           IconButton(
-            icon: Icon(Icons.arrow_back),
+              icon: Icon(Icons.arrow_back),
             onPressed: (){
               Navigator.pop(context);
             }
@@ -42,14 +59,15 @@ class _LocationAppBarState extends State<LocationAppBar> {
             ),
           ): Container(),
           widget.withCurrentLocation? SizedBox(height: size.height * 0.01,): Container(),
-          Text(
-            widget.location,
-            style: TextStyle(
-              fontSize: 25,
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
+          widget.withCurrentLocation 
+            ? Text(
+                location, 
+                style: TextStyle(fontSize: 25, color: Colors.black, fontWeight: FontWeight.bold)
+              ) 
+            : Text(
+                widget.location, 
+                style: TextStyle(fontSize: 25, color: Colors.black, fontWeight: FontWeight.bold)
+              ),
         ],
       ),
     );
